450a2ab15f1e322670859f06068ef75638c82e0d
kbob <kbob@jogger-egg.com>
2020-04-24 17:56:24 -0400

    Partially implement Voice class.

--- synth/core/test-voice.h
+++ synth/core/test-voice.h
@@ -4,6 +4,11 @@
 
 #include "synth/core/config.h"
 
+class FooControl : public ControlType<FooControl> {
+public:
+    void render(size_t) {}
+};
+
 class FooModule : public ModuleType<FooModule> {
 public:
     Input<> in;
@@ -11,7 +16,7 @@ public:
     void render(size_t) {}
 };
 
-class VoiceUnitTest : public CxxTest::TestSuite {
+class voice_unit_test : public CxxTest::TestSuite {
 
 public:
 
@@ -20,24 +25,36 @@ public:
         (void)Voice();
     }
 
+    void test_add_control()
+    {
+        Voice v;
+        FooControl foo;
+        v.add_control(&foo);
+
+        const Voice::control_vector& m = v.controls();
+        TS_ASSERT(m.size() == 1);
+        TS_ASSERT(m.at(0) == &foo);
+        TS_ASSERT(m.capacity() <= MAX_VOICE_CONTROLS);
+    }
+
     void test_add_module()
     {
         Voice v;
         FooModule foo;
-        v.module(&foo);
+        v.add_module(&foo);
 
         const Voice::module_vector& m = v.modules();
         TS_ASSERT(m.size() == 1);
         TS_ASSERT(m.at(0) == &foo);
-        TS_ASSERT(m.capacity() <= MAX_MODULES);
+        TS_ASSERT(m.capacity() <= MAX_VOICE_MODULES);
     }
 
     void test_two_modules()
     {
         Voice v;
         FooModule foo1, foo2;
-        v.module(&foo1);
-        v.module(&foo2);
+        v.add_module(&foo1);
+        v.add_module(&foo2);
 
         auto m = v.modules();
         TS_ASSERT(m.size() == 2);
@@ -45,22 +62,27 @@ public:
         TS_ASSERT(m.at(1) == &foo2);
     }
 
-    // XXX test port enumeration
-
     void test_clone()
     {
         Voice v;
-        FooModule foo1, foo2;
-        v.module(&foo1);
-        v.module(&foo2);
+        FooControl c1, c2;
+        FooModule m1, m2;
+        v.add_control(&c1);
+        v.add_control(&c2);
+        v.add_module(&m1);
+        v.add_module(&m2);
         Voice *v2 = v.clone();
 
-        auto m2 = v2->modules();
-        TS_ASSERT(m2.size() == 2);
-        TS_ASSERT(m2.at(0) != &foo1 && m2.at(0) != &foo2);
-        TS_ASSERT(dynamic_cast<FooModule *>(m2.at(0)));
-        TS_ASSERT(m2.at(1) != &foo1 && m2.at(1) != &foo2);
-        TS_ASSERT(dynamic_cast<FooModule *>(m2.at(1)));
+        auto ctls = v2->controls();
+        TS_ASSERT_EQUALS(ctls.size(), 2);
+        TS_ASSERT(ctls.at(0) != &c1 && ctls.at(0) != &c2);
+        TS_ASSERT(dynamic_cast<FooControl *>(ctls.at(0)));
+        auto mods = v2->modules();
+        TS_ASSERT(mods.size() == 2);
+        TS_ASSERT(mods.at(0) != &m1 && mods.at(0) != &m2);
+        TS_ASSERT(dynamic_cast<FooModule *>(mods.at(0)));
+        TS_ASSERT(mods.at(1) != &m1 && mods.at(1) != &m2);
+        TS_ASSERT(dynamic_cast<FooModule *>(mods.at(1)));
     }
 
 };
--- synth/core/voice.h
+++ synth/core/voice.h
@@ -1,39 +1,80 @@
 #ifndef VOICE_included
 #define VOICE_included
 
-#include <vector>
-
+#include "synth/core/action.h"
 #include "synth/core/config.h"
+#include "synth/core/controls.h"
 #include "synth/core/modules.h"
 #include "synth/core/plan.h"
 #include "synth/util/noalloc.h"
+
+class Timbre;
+
+// -- Voice -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- //
+//
+// A Voice has:
+//     a state
+//     an owning timbre (which changes)
+//     some controls
+//     some modules
+//     a render action sequence
+//
+// A Voice can:
+//     start, release, and kill a note.
 
 class Voice {
 
 public:
 
-    typedef fixed_vector<Module *, MAX_MODULES> module_vector;
+    enum class State {
+        IDLE,
+        ACTIVE,
+        RELEASING,
+        STOPPING,
+    };
 
-    void module(Module *m)
-    {
-        m_modules.push_back(m);
-    }
+    typedef fixed_vector<Control *, MAX_VOICE_CONTROLS> control_vector;
+    typedef fixed_vector<Module *, MAX_VOICE_MODULES> module_vector;
+
+    Voice()
+    : m_state(State::IDLE) {}
+    ~Voice() = default;
 
     Voice *clone() const
     {
         Voice *v = new Voice();
+        for (const auto *c: m_controls)
+            v->add_control(c->clone());
         for (const auto *m: m_modules)
-            v->module(m->clone());
+            v->add_module(m->clone());
         return v;
     }
 
-    void init(Plan *plan) { m_plan = plan; }
+    State state() const { return m_state; }
+
+    Timbre *timbre() const { return m_timbre;}
+    void timbre(Timbre *t) { m_timbre = t; }
+
+    const control_vector& controls() const { return m_controls; }
+    void add_control(Control *c) { m_controls.push_back(c); }
+
     const module_vector& modules() const { return m_modules; }
+    void add_module(Module *m) { m_modules.push_back(m); }
+
+    const render_action_sequence actions() const { return m_actions; }
+    void actions(render_action_sequence&& a) { m_actions = a; }
+
+    void start_note();
+    void release_note();
+    void kill_note();
 
 private:
 
+    State m_state;
+    Timbre *m_timbre;
+    control_vector m_controls;
     module_vector m_modules;
-    Plan *m_plan;
+    render_action_sequence m_actions;
 
     friend class VoiceUnitTest;
 
