98c9b1b771715cfa687202bb8c270b016f203760
kbob <kbob@jogger-egg.com>
2020-04-25 08:12:27 -0400

    implement Timbre.

--- synth/core/test-timbre.h
+++ synth/core/test-timbre.h
@@ -2,13 +2,130 @@
 
 #include <cxxtest/TestSuite.h>
 
-class TimbreUnitTest : public CxxTest::TestSuite {
+class FooControl : public ControlType<FooControl> {
+public:
+    void render(size_t) {}
+};
+
+class FooModule : public ModuleType<FooModule> {
+public:
+    Input<> in;
+    Output<> out;
+    void render(size_t) {}
+};
+
+class Patch {};                 // XXX these will probably break...
+class Plan {};                  // XXX
+
+class timbre_unit_test : public CxxTest::TestSuite {
 
 public:
 
-    void test_instantiage()
+    void test_instantiate()
     {
         (void)Timbre();
+    }
+
+    void test_copy()
+    {
+        FooControl *c = new FooControl;
+        FooModule *m = new FooModule;
+        Timbre t;
+        t.add_control(c);
+        t.add_module(m);
+        Timbre t2 = t;
+
+        auto ctls = t2.controls();
+        TS_ASSERT_EQUALS(ctls.size(), 1);
+        TS_ASSERT(ctls.at(0) != c);
+        TS_ASSERT(dynamic_cast<FooControl *>(ctls.at(0)));
+        auto mods = t2.modules();
+        TS_ASSERT(mods.size() == 1);
+        TS_ASSERT(mods.at(0) != m);
+        TS_ASSERT(dynamic_cast<FooModule *>(mods.at(0)));
+    }
+
+    void test_patch()
+    {
+        Patch c, d;
+        Timbre t;
+        TS_ASSERT_EQUALS(t.current_patch(), nullptr);
+        TS_ASSERT_EQUALS(t.default_patch(), nullptr);
+        t.default_patch(&d);
+        t.set_patch(&c);
+        TS_ASSERT_EQUALS(t.current_patch(), &c);
+        TS_ASSERT_EQUALS(t.default_patch(), &d);
+        t.set_patch(nullptr);
+        TS_ASSERT_EQUALS(t.current_patch(), nullptr);
+    }
+
+    void test_plan()
+    {
+        Plan p;
+        Timbre t;
+        TS_ASSERT_EQUALS(t.plan(), nullptr);
+        t.plan(&p);
+        TS_ASSERT_EQUALS(t.plan(), &p);
+        t.plan(nullptr);
+        TS_ASSERT_EQUALS(t.plan(), nullptr);
+    }
+
+    void test_add_control()
+    {
+        Timbre t;
+        FooControl *foo{new FooControl};
+        t.add_control(foo);
+
+        const Timbre::control_vector& cv = t.controls();
+        TS_ASSERT(cv.size() == 1);
+        TS_ASSERT(cv.at(0) == foo);
+        TS_ASSERT(cv.capacity() <= MAX_VOICE_CONTROLS);
+    }
+
+    void test_add_module()
+    {
+        Timbre t;
+        FooModule *foo{new FooModule};
+        t.add_module(foo);
+
+        const Timbre::module_vector& mv = t.modules();
+        TS_ASSERT(mv.size() == 1);
+        TS_ASSERT(mv.at(0) == foo);
+        TS_ASSERT(mv.capacity() <= MAX_VOICE_MODULES);
+    }
+
+    void test_pre_render()
+    {
+        std::stringstream log;
+        auto a0 = [&] (size_t n) {
+            log << 'a' << n << ' ';
+        };
+        auto a1 = [&] (size_t n) {
+            log << 'b' << n << ' ';
+        };
+        render_action_sequence seq{a0, a1};
+        Timbre t;
+        t.pre_actions(seq);
+        TS_ASSERT_EQUALS(t.pre_actions().size(), 2);
+        t.pre_render(10);
+        TS_ASSERT_EQUALS(log.str(), "a10 b10 ");
+    }
+
+    void test_post_render()
+    {
+        std::stringstream log;
+        auto a0 = [&] (size_t n) {
+            log << 'a' << n << ' ';
+        };
+        auto a1 = [&] (size_t n) {
+            log << 'b' << n << ' ';
+        };
+        render_action_sequence seq{a0, a1};
+        Timbre t;
+        t.post_actions(seq);
+        TS_ASSERT_EQUALS(t.post_actions().size(), 2);
+        t.post_render(10);
+        TS_ASSERT_EQUALS(log.str(), "a10 b10 ");
     }
 
 };
--- synth/core/timbre.h
+++ synth/core/timbre.h
@@ -1,31 +1,112 @@
 #ifndef TIMBRE_included
 #define TIMBRE_included
 
+#include "synth/core/action.h"
 #include "synth/core/config.h"
+#include "synth/core/controls.h"
+#include "synth/core/modules.h"
 #include "synth/util/noalloc.h"
 
-class Control;
-class Module;
 class Patch;
 class Plan;
 
-// A Timbre has a patch reference, a "factory default" patch,
-// a plan, per-timbre controls, per-timbre modules, and pre-
-// and post-voice render actions.
+// A Timbre has:
+//     a patch reference
+//     a "factory default" patch
+//     a plan
+//     per-timbre controls
+//     per-timbre modules
+//     a pre-voice render action sequence
+//     a post-voice render action sequence
+//
+// A Timbre can:
+//     pre-render a frame chunk.
+//     post-render a frame chunk.
 
 class Timbre {
 public:
 
-    Timbre() = default;
+    typedef fixed_vector<Control *, MAX_TIMBRE_CONTROLS> control_vector;
+    typedef fixed_vector<Module *, MAX_TIMBRE_MODULES> module_vector;
 
-// private:
+    Timbre()
+    : m_current_patch{nullptr},
+      m_default_patch{nullptr},
+      m_plan{nullptr},
+      m_controls{},
+      m_modules{},
+      m_pre_actions{},
+      m_post_actions{}
+    {}
+
+    Timbre(const Timbre& that)
+    : m_current_patch{that.m_current_patch},
+      m_default_patch{that.m_default_patch},
+      m_plan{that.m_plan},
+      m_controls{},
+      m_modules{},
+      m_pre_actions{},
+      m_post_actions{}
+    {
+        for (auto *c: that.m_controls)
+            m_controls.push_back(c->clone());
+        for (auto *m: that.m_modules)
+            m_modules.push_back(m->clone());
+    }
+
+    Timbre& operator = (const Timbre&) = delete;
+
+    ~Timbre()
+    {
+        while (!m_modules.empty()) {
+            delete m_modules.back();
+            m_modules.pop_back();
+        }
+    }
+
+    Patch *current_patch() const { return m_current_patch; }
+    void set_patch(Patch *p) { m_current_patch = p; }
+
+    Patch *default_patch() const { return m_default_patch; }
+    void default_patch(Patch *p) { m_default_patch = p; }
+
+    Plan *plan() const { return m_plan; }
+    void plan(Plan *p) { m_plan = p; }
+
+    const control_vector& controls() const { return m_controls; }
+    void add_control(Control *c) { m_controls.push_back(c); }
+
+    const module_vector& modules() const { return m_modules; }
+    void add_module(Module *m) { m_modules.push_back(m); }
+
+    const render_action_sequence pre_actions() const { return m_pre_actions; }
+    void pre_actions(const render_action_sequence& a) { m_pre_actions = a; }
+
+    const render_action_sequence post_actions() const { return m_post_actions; }
+    void post_actions(const render_action_sequence& a) { m_post_actions = a; }
+
+    void pre_render(size_t frame_count) const
+    {
+        for (auto& a: m_pre_actions)
+            a(frame_count);
+    }
+
+    void post_render(size_t frame_count) const
+    {
+        for (auto& a: m_post_actions)
+            a(frame_count);
+    }
+
+private:
     Patch *m_current_patch;
     Patch *m_default_patch;
     Plan *m_plan;
-    fixed_vector<Control *, MAX_TIMBRE_CONTROLS> m_controls;
-    fixed_vector<Module *, MAX_TIMBRE_MODULES> m_modules;
-    // fixed_vector<RenderAction, MAX_RENDER_ACTION> m_pre_actions;
-    // fixed_vector<RenderAction, MAX_RENDER_ACTION> m_post_actions;
+    control_vector m_controls;
+    module_vector m_modules;
+    render_action_sequence m_pre_actions;
+    render_action_sequence m_post_actions;
+
+    friend class timbre_unit_test;
 
 };
 
